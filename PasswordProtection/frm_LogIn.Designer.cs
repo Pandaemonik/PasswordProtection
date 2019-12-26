b09df7ed3ef3da3c9ce92f9989d8c8360069d29e
Pandaemonik <zero.cold1000@gmail.com>
2019-12-26 02:10:18 +0200

    v0.2

--- PasswordProtection/frm_LogIn.Designer.cs
+++ PasswordProtection/frm_LogIn.Designer.cs
@@ -105,6 +105,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(316, 170);
+            this.ControlBox = false;
             this.Controls.Add(this.btn_Register);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btn_LogIn);
